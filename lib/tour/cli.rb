63d1d10f65073b53c8413a92472cb4197ba1fa9c
Stela Capsa <stela.capsa@gmail.com>
2020-02-10 17:57:57 +0000

    changes in cli

--- lib/tour/cli.rb
+++ lib/tour/cli.rb
@@ -10,6 +10,7 @@ class Tour::ClI
     puts "|                                       |"
     puts "|---------------------------------------|"
     web
+    calendar
   end
   
   
@@ -22,5 +23,48 @@ results = page.css("div.sample-itineraries-module--sample-itineraries__itinerary
 puts results.count
 end 
   
+  
+def calendar
+  puts "please type in your month (ex:full name january)"
+  month = gets.chomp
+  puts "please type in your date(numbers)"
+  date = gets.to_i
+  puts "|----------------------------------------------|"
+  puts "|Your tour is booked for #{date} of #{month}!  |"
+  puts "|----------------------------------------------|"
+  
+  if month == "november"||"decembre"||"january"||"february"||"march"
+    puts tour1
+       elsif month == "april"||"may"||"june"||"july"||"august"||"september"||"octombre"
+       puts tour2
+         else 
+         puts "error"
+  end 
+end 
+  
+  def tour1
+    puts "|tour schedule|"
+    puts "|-----------------------------------------------------------|"
+    puts "|1. pick up at 7 am                                         |"
+    puts "|2. three places to visit                                   |"
+    puts "|3. lunch 1 pm local food                                   |"
+    puts "|4. four more places to visit                               |"
+    puts "|1. dinner, restaurant located on the cliff of the mountain |"
+    puts "|-----------------------------------------------------------|"
+  end 
+
+
+def tour2
+    puts "|tour schedule|"
+    puts "|-----------------------------------------------------------|"
+    puts "|1. pick up at 7 am                                         |"
+    puts "|2. three places to visit                                   |"
+    puts "|3. lunch 1 pm local food                                   |"
+    puts "|4. four more places to visit, bike ride includet           |"
+    puts "|1. dinner, on the yacht with beautiful sunset              |"
+    puts "|-----------------------------------------------------------|"
+  end 
+  
+
   
 end 
